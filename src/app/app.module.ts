fdda21c8149fa51700529e6526848223af2ace22
VladanStar <vladancupric@gmail.com>
2023-03-28 19:12:37 +0200

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-
+import{MatDatepickerModule}from"@angular/material/datepicker";
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,6 +40,8 @@ import { FormsModule } from '@angular/forms';
     FlexLayoutModule,
     MatFormFieldModule,
     MatInputModule,
+MatDatepickerModule,
+MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
