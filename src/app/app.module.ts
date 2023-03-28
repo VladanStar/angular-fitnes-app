4a4edab9921f887437a9982b2c50022135327428
VladanStar <vladancupric@gmail.com>
2023-03-28 15:17:57 +0200

    Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { TrainingComponent } from './components/training/training.component';
+import { PreviousTrainingComponent } from './components/training/previous-training/previous-training.component';
+import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
+import { NewTrainingComponent } from './components/training/new-training/new-training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    WelcomeComponent,
+    TrainingComponent,
+    PreviousTrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/training/previous-training/previous-training.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-previous-training',
+  templateUrl: './previous-training.component.html',
+  styleUrls: ['./previous-training.component.css']
+})
+export class PreviousTrainingComponent {
+
+}
