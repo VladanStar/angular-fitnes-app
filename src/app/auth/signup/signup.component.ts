42423faeef474253a44fa20931817948658ee7fb
VladanStar <vladancupric@gmail.com>
2023-03-28 18:35:51 +0200

    Add

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,7 +8,8 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
-onSubmit(f:any){
+onSubmit(form:NgForm){
+console.log(form)
 
 }
 }
